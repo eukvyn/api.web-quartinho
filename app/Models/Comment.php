3bc42e38efc2ed8b9221bba704f96dd1906dc7de
Kevin Almeida <janioklalmeida05@gmail.com>
2024-02-28 19:39:24 -0300

    feat: format create date for comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Carbon;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $hidden = ['updated_at', 'created_at'];
 
     protected $fillable = [
         'text',
@@ -24,5 +26,12 @@ class Comment extends Model
     public function property()
     {
         return $this->belongsTo(Property::class);
+    }
+
+    protected $appends = ['created_at_formatted'];
+
+    public function getCreatedAtFormattedAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])->format('d/m/Y');
     }
 }
